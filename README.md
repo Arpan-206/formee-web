f1a695e7f369b95a1bc64e7a4087317a1108fdd7
Arpan Pandey <arpanpandey.aps@gmail.com>
2022-03-29 11:09:51 +0530

    Minor Docs Changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# formee-web
+# Formee Web
+
+![Formee Logo](public/logo.svg)
+
+This is a WebApp for the formee CLI to make it easy to fill out forms with a modern interface.
+
+Please visit the [main project page](https://github.com/Arpan-206/formee) for further information.
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import 'carbon-components/scss/globals/scss/styles.scss';
+import "carbon-components/scss/globals/scss/styles.scss";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Formee</title>
+        <link rel="icon" href="/logo.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
