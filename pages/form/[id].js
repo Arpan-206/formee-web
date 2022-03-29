7832661a486ba20510b429f2834c3aea83961c3e
Arpan Pandey <arpanpandey.aps@gmail.com>
2022-03-29 07:01:57 +0530

    Fixed API thingy in prod

--- pages/form/[id].js
+++ pages/form/[id].js
@@ -171,7 +171,15 @@ export default function Home({ data, tokenJson }) {
 
 export async function getServerSideProps(context) {
   const { id } = context.query;
-  const token = await fetch("https://formee-auth.hackersreboot.tech/visitor");
+  const token = await fetch("https://formee-auth.hackersreboot.tech/visitor", {
+        method: "GET",
+        headers: {
+          // update with your user-agent
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36", 
+          Accept: "application/json; charset=UTF-8",
+        },
+      });
   const tokenJson = await token.json();
   const client = new ApolloClient({
     uri: "https://hrbt-portal.hasura.app/v1/graphql/",
